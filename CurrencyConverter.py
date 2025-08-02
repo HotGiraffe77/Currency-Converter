6e38f2c8eda8ae5df3ad5f13810ee29f2bd9b6bc
HotGiraffe77 <170359653+HotGiraffe77@users.noreply.github.com>
2025-08-02 13:07:17 +0100

    Added live conversion rates through v6.exchangerate.api

--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,41 +1,61 @@
-import random
 import tkinter as tk
+import requests
 
-EXCHANGE_RATE = 0.005
-
-def convertJPY():
+def convert_currency():
     try:
-        yen = float(entry_yen.get())
-        gbp = yen * EXCHANGE_RATE
-        label_result.config(text=f"{gbp:.2f} GBP")
+        amount = float(entry_amount.get())
+        from_currency = from_var.get()
+        to_currency = to_var.get()
+
+
+        api_key = "0ca2f23e91736262851de6bd"
+        url = f"https://v6.exchangerate-api.com/v6/{api_key}/pair/{from_currency}/{to_currency}"
+        response = requests.get(url)
+        print("Status code:", response.status_code)
+        print("Raw Resposne:", response.text)
+
+        data = response.json()
+
+        if data["result"] != "success":
+            label_result.config(text="API error: check currency codes or key")
+            return
+
+        rate = data["conversion_rate"]
+        result = amount * rate
+        label_result.config(text=f"{result:.2f} {to_currency}")
+
+
     except ValueError:
-        label_result.config(text="Invalid Input...")
+        label_result.config(text="Invalid amount")
+    except Exception as e:
+        label_result.config(text=f"Error: {e}")
+        print("Exception:", e)
 
-def convertGBP():
-    try:
-        gbp = float(entry_yen.get())
-        yen = gbp / EXCHANGE_RATE
-        label_result.config(text=f"{yen:,.2f} JPY")
-    except ValueError:
-        label_result.config(text="Invalid Input...")
-
-
-
+# GUI Setup
 root = tk.Tk()
 root.title("Currency Converter")
-root.geometry("900x600")
+root.geometry("300x250")
 
-tk.Label(root, text="GBP/JPY Converter").pack(pady=50)
-entry_yen = tk.Entry(root)
-entry_yen.pack()
+# Input
+tk.Label(root, text="Amount:").pack(pady=(10, 0))
+entry_amount = tk.Entry(root)
+entry_amount.pack()
 
-tk.Button(root, text="GBP -> JPY", command=convertGBP).pack(side="top", pady = (50, 10))
+# From Currency
+tk.Label(root, text="From:").pack()
+from_var = tk.StringVar(value="USD")
+tk.OptionMenu(root, from_var, "USD", "EUR", "GBP", "INR", "JPY", "CAD").pack()
 
-tk.Button(root, text="JPY -> GBP", command=convertJPY).pack(side="top", pady=(10, 50))
+# To Currency
+tk.Label(root, text="To:").pack()
+to_var = tk.StringVar(value="EUR")
+tk.OptionMenu(root, to_var, "USD", "EUR", "GBP", "INR", "JPY", "CAD").pack()
 
+# Convert Button
+tk.Button(root, text="Convert", command=convert_currency).pack(pady=10)
 
-
-label_result = tk.Label(root, text="Result will appear here...")
-label_result.pack()
+# Result Label
+label_result = tk.Label(root, text="Result will appear here")
+label_result.pack(pady=(10, 0))
 
 root.mainloop()
